b24e8f9a4939454e43c46f5d2159b418ce71e68a
Kishor-gn14 <kishor142521@gmail.com>
2024-06-28 07:27:32 +0530

    encapsulation assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+        // rwOnly.name = "Kishor"; // This line will cause an error
+        // System.out.println(rwOnly.name); // This line will cause an error
+
+        rwOnly.setName("Kishor");
+        System.out.println(rwOnly.getName());
+    }
 }
